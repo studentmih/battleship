2f99c32668a579de5218439e6028bad00091f4bf
mh <862265611@qq.com>
2018-05-18 11:35:42 +0800

    获得一艘战舰，查看guess是否该战舰占据的位置之一，判断是否击中，合并简化代码

--- battleship.js
+++ battleship.js
@@ -12,6 +12,39 @@ var view = {
     cell.setAttribute("class","miss");
   }
 };
-var ship1 = [{ location: ["10", "20", "30"],hits:["","",""]},
-            { location: ["32", "33", "34"],hits:["","",""]},
-            { location: ["63", "64", "65"],hits:["","","hit"]}];
+var model = {
+  boardSize: 7,
+  sumShips: 3,
+  shipsSunk: 0,
+  shipLength: 3,
+  ships:[{ location: ["10", "20", "30"],hits:["","",""]    },
+              { location: ["32", "33", "34"],hits:["","",""]   },
+              { location: ["63", "64", "65"],hits:["","","hit"]}],
+  fire: function (guess){
+    for(var i = 0;i < this.numShips; i++){
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if(index >= 0){
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT");
+        if(this.isSunk(ship)){
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+  isSunk:function(ship){
+    for(var i = 0; i < this.shipLength; i++){
+      if(ship.hits[i]!=="hit"){
+        return false;
+      }
+    }
+    return true;
+  }
+},
